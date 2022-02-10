7498d4a436421e7f4c841e4339b1b58d9e445cfd
yrys_NM <yrys.n3m@gmail.com>
2022-02-11 05:02:04 +0600

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from django.utils import timezone
+
+from .models import Post
+""" Точка перед models означает текущую директорию или текущее приложение. 
+	Поскольку views.py и models.py находятся в одной директории, мы можем использовать точку . и имя файла (без расширения .py). 
+	Затем мы импортируем модель (Post).
+"""
+
+
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by("published_date")
+	return render(request, 'blog/post_list.html', {'posts': posts})
